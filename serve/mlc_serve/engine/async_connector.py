58ccc239166e7e6f87e60488e62898996f3c52f8
Jared Roesch <jroesch@octoml.ai>
2023-12-23 01:53:38 +0000

    Fix

--- serve/mlc_serve/engine/async_connector.py
+++ serve/mlc_serve/engine/async_connector.py
@@ -18,27 +18,6 @@ LOG = structlog.stdlib.get_logger(__name__)
 
 ResultQueue = asyncio.Queue[RequestOutput]
 
-import contextvars
-import functools
-from asyncio import get_running_loop
-
-CANCELLATION_EXECUTOR = ThreadPoolExecutor(None)
-
-async def to_thread_with_cancel_pool(func, /, *args, **kwargs):
-    """Asynchronously run function *func* in a separate thread.
-
-    Any *args and **kwargs supplied for this function are directly passed
-    to *func*. Also, the current :class:`contextvars.Context` is propagated,
-    allowing context variables from the main thread to be accessed in the
-    separate thread.
-
-    Return a coroutine that can be awaited to get the eventual result of *func*.
-    """
-    loop = get_running_loop()
-    ctx = contextvars.copy_context()
-    func_call = functools.partial(ctx.run, func, *args, **kwargs)
-    return await loop.run_in_executor(CANCELLATION_EXECUTOR, func_call)
-
 class AsyncEngineConnector:
     def __init__(self, engine: InferenceEngine, engine_wait_timeout=1):
         self.engine = engine
