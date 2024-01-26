d3f28e57ce133e4828e23e251ba2c4c595101a0a
Luis Vega <lvega@octoml.ai>
2024-01-26 22:18:25 +0000

    added json mode to latency and throughput benchmark

--- serve/benchmarks/utils.py
+++ serve/benchmarks/utils.py
@@ -1,4 +1,8 @@
 """Utils for benchmark scripts"""
+from pydantic import BaseModel
+
+class Output(BaseModel):
+    answer: str
 
 
 def add_sampling_flags(parser):
@@ -18,6 +22,11 @@ def add_sampling_flags(parser):
         help="Apply top-p and top-k.",
     )
     parser.add_argument(
+        "--apply-json-mode",
+        action="store_true",
+        help="Apply json mode.",
+    )
+    parser.add_argument(
         "--apply-all-sampling-params",
         action="store_true",
         help="Apply all penalties, logit bias, top-p and top-k.",
@@ -33,12 +42,14 @@ def postproc_sampling_args(args):
         "repetition_penalty": 1.0,
         "top_p": 1.0,
         "top_k": -1,
+        "json_schema": None
     }
 
     if args.apply_all_sampling_params:
         args.apply_penalties = True
         args.apply_logit_bias = True
         args.apply_top_p_top_k = True
+        args.apply_json_mode = True
 
     if args.apply_penalties:
         args.sampling_setting["presence_penalty"] = 0.7
@@ -51,3 +62,6 @@ def postproc_sampling_args(args):
     if args.apply_top_p_top_k:
         args.sampling_setting["top_k"] = 2
         args.sampling_setting["top_p"] = 0.7
+
+    if args.apply_json_mode:
+        args.sampling_setting["json_schema"] = Output.model_json_schema()
