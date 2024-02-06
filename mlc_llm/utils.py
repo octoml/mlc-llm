4f889597367529c47a2a44af61945276379a51dd
Eric Lunderberg <elunderberg@octoml.ai>
2024-02-06 20:50:45 +0000

    [Debug] Output `original_params` directory
    For debug purposes, symlink all `*.safetensors` files into the
    `original_params` directory of the output.

--- mlc_llm/utils.py
+++ mlc_llm/utils.py
@@ -733,3 +733,29 @@ def parse_target(args: argparse.Namespace) -> None:
         args.lib_format = "dll"
 
     print(f"Target configured: {args.target}")
+
+
+def symlink_original_params(
+    artifact_path: Union[str, pathlib.Path], model_path: Union[str, pathlib.Path]
+):
+    artifact_path = pathlib.Path(artifact_path)
+    model_path = pathlib.Path(model_path)
+
+    symlink_dirpath = artifact_path.joinpath("original_params")
+
+    if symlink_dirpath.exists():
+        assert symlink_dirpath.is_dir()
+    else:
+        symlink_dirpath.mkdir()
+
+    for safetensors_filepath in model_path.glob("*.safetensors"):
+        symlink_filepath = symlink_dirpath.joinpath(safetensors_filepath.name)
+
+        if symlink_filepath.exists():
+            assert symlink_filepath.resolve() == safetensors_filepath.resolve(), (
+                f"Attempted to make a symlink from {symlink_dirpath} "
+                f"to {safetensors_filepath}, "
+                f"but {model_path} already exists"
+            )
+        else:
+            symlink_filepath.symlink_to(safetensors_filepath.resolve())
